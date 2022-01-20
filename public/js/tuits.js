4acd833e39cdbcd59d316bc20fb02125b72fd7f5
Nick Osborn <nicholas.p.osborn@gmail.com>
2022-01-20 12:18:24 -0500

    compile no errors

--- public/js/tuits.js
+++ public/js/tuits.js
@@ -1,21 +1,24 @@
 import tuits from "./tuits-data.js";
 
 function Tuits() {
- return(`
+    return `
 <ul class="ttr-tuits list-group">
- ${
-   tuits.map(tuit => {
-     return(`
+ ${tuits
+     .map((tuit) => {
+         return `
        <li class="p-2 ttr-tuit list-group-item d-flex">
         <div class="pe-2">
-         <img src="../images/${tuit['avatar-logo']}"
+         <img src="../images/${tuit["avatar-logo"]}"
               class="ttr-tuit-avatar-logo rounded-circle"/>
         </div>
         <div>
-         <h2 class="fs-5">${tuit.username} @${tuit.handle} - ${tuit.published}</h2>
+         <h2 class="fs-5">${tuit.username} @${tuit.handle} - ${
+             tuit.published
+         }</h2>
          ${tuit.content}
          ${
-           tuit.youtube ? `
+             tuit.youtube
+                 ? `
            <div class="ttr-responsive-video ttr-rounded-15px position-relative overflow-hidden w-100 mt-2">
              <iframe width="560"
                      height="315" src="${tuit.youtube}"
@@ -25,24 +28,29 @@ function Tuits() {
                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                      allowfullscreen></iframe>
             </div>
-        `: ''
-          }
+        `
+                 : ""
+         }
          
          ${
-           tuit.image ? `
+             tuit.image
+                 ? `
              <div class="position-relative">
               <img src="../images/${tuit.image}"
                    class="mt-2 w-100 ttr-rounded-15px"/>
               ${
-                tuit['image-overlay'] ? `
+                  tuit["image-overlay"]
+                      ? `
                   <span class="fa-2x text-white fw-bold bottom-0 ttr-tuit-image-overlay position-absolute">
-                      ${tuit['image-overlay']}
+                      ${tuit["image-overlay"]}
                   </span>
-                `: ''
+                `
+                      : ""
               }
              </div>
-           `: ''
-          }
+           `
+                 : ""
+         }
          <div class="row mt-2">
           <div class="col">
            <i class="far fa-message"></i>
@@ -62,10 +70,10 @@ function Tuits() {
          </div>
         </div>
        </li>
-     `);
-   }).join('')
- }
+     `;
+     })
+     .join("")}
  </ul>
-    `);
+    `;
 }
 export default Tuits;
--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -1,18 +1,14 @@
 import tuits from "./tuits-data.json";
-import './tuits.css';
+import "./tuits.css";
 import Tuit from "./tuit";
 
 function Tuits() {
- return(
-<ul class="ttr-tuits list-group">
- {
-   tuits.map(tuit => {
-     return(
-       <Tuit tuit={tuit}/>
-     );
-   })
- }
-</ul>
+    return (
+        <ul className="ttr-tuits list-group">
+            {tuits.map((tuit, i) => {
+                return <Tuit tuit={tuit} key={i} />;
+            })}
+        </ul>
     );
 }
 export default Tuits;
