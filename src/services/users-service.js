0e4e5ab0097fb93f38619111a82201c668e7dd5f
Nick Osborn <nicholas.p.osborn@gmail.com>
2022-03-08 13:55:24 -0500

    fixed users service

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 const BASE_URL = "https://cs5500a3.herokuapp.com/api";
-// const BASE_URL = "http://localhost:4000/api";
 
 const LOGIN_API = `${BASE_URL}/login`;
 const USERS_API = `${BASE_URL}/users`;
--- src/tests/tuits-service.test.js
+++ src/tests/tuits-service.test.js
@@ -1,15 +1,47 @@
-describe('can create tuit with REST API', () => {
-  // TODO: implement this
+import {
+    createTuit,
+    findAllTuits,
+    findTuitById,
+    findTuitByUser,
+    updateTuit,
+    deleteTuit,
+} from "../services/tuits-service";
+
+import { createUser, deleteUsersByUsername } from "../services/users-service";
+
+describe("can create tuit with REST API", () => {
+    const testUser = {
+        username: "ellenripley",
+        password: "password",
+        email: "testuser@user.com",
+    };
+    const testTuit = { tuit: "this is test text for a test tuit" };
+
+    let newUser;
+    beforeAll(async () => {
+        newUser = await createUser(testUser);
+        return newUser;
+    });
+
+    afterAll(() => {
+        return deleteUsersByUsername(testUser.username);
+    });
+
+    test("can insert tuit with defined user", async () => {
+        const tuit = await createTuit(newUser.uid, testTuit);
+
+        expect(tuit.tuit).toEqual(testTuit.tuit);
+    });
 });
 
-describe('can delete tuit wtih REST API', () => {
-  // TODO: implement this
+describe("can delete tuit wtih REST API", () => {
+    // TODO: implement this
 });
 
-describe('can retrieve a tuit by their primary key with REST API', () => {
-  // TODO: implement this
+describe("can retrieve a tuit by their primary key with REST API", () => {
+    // TODO: implement this
 });
 
-describe('can retrieve all tuits with REST API', () => {
-  // TODO: implement this
+describe("can retrieve all tuits with REST API", () => {
+    // TODO: implement this
 });
