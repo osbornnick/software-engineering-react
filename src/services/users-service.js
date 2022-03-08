0ebd89f8278c4a2906c4ae392ff313581042f576
Nick Osborn <nicholas.p.osborn@gmail.com>
2022-03-08 10:49:57 -0500

    use a3 version of API

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://cs5500a2.herokuapp.com/api";
+const BASE_URL = "https://cs5500a3.herokuapp.com/api";
 // const BASE_URL = "http://localhost:4000/api";
 
 const LOGIN_API = `${BASE_URL}/login`;
