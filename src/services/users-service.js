bc7e54ad197192e4a3049f1da62df8c068c9eaf7
Nick Osborn <nicholas.p.osborn@gmail.com>
2022-04-01 16:54:15 -0400

    use axios with credentials

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -4,20 +4,24 @@ import axios from "axios";
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 const USERS_API = `${BASE_URL}/api/users`;
 
+const api = axios.create({
+    withCredentials: true,
+});
+
 export const createUser = (user) =>
-    axios.post(`${USERS_API}`, user).then((response) => response.data);
+    api.post(`${USERS_API}`, user).then((response) => response.data);
 
 export const findAllUsers = () =>
-    axios.get(USERS_API).then((response) => response.data);
+    api.get(USERS_API).then((response) => response.data);
 
 export const findUserById = (uid) =>
-    axios.get(`${USERS_API}/${uid}`).then((response) => response.data);
+    api.get(`${USERS_API}/${uid}`).then((response) => response.data);
 
 export const deleteUser = (uid) =>
-    axios.delete(`${USERS_API}/${uid}`).then((response) => response.data);
+    api.delete(`${USERS_API}/${uid}`).then((response) => response.data);
 
 export const deleteUsersByUsername = (username) =>
-    axios
+    api
         .get(`${USERS_API}/username/${username}/delete`)
         .then((response) => response.data);
 
